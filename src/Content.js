f9b525f9433eceec913e19bc2b8bc66707323bac
Teja <saitejanomula143@gmail.com>
2025-06-01 14:42:45 +0530

    see update

--- src/Content.js
+++ src/Content.js
@@ -15,7 +15,7 @@ import services_logo1 from "./assets/images/Services/logo1.png";
 import services_logo2 from "./assets/images/Services/logo2.png";
 import services_logo3 from "./assets/images/Services/logo3.png";
 
-// Project images
+
 import project1 from "./assets/images/projects/img1.png";
 import project2 from "./assets/images/projects/img2.png";
 import project3 from "./assets/images/projects/img3.png";
