77a74857b5dd78040bac094e95593800e0b8db1f
Teja <saitejanomula143@gmail.com>
2025-06-01 14:50:45 +0530

    delete and update new

--- src/Content.js
+++ src/Content.js
@@ -17,10 +17,10 @@ import services_logo3 from "./assets/images/Services/logo3.png";
 
 
 
-import project1 from "./assets/images/projects/img1.png"; 
-import project2 from "./assets/images/projects/img2.png";
-import project3 from "./assets/images/projects/img3.png";
-import person_project from "./assets/images/projects/person.png";
+import project1 from "./assets/images/project/img1.png";
+import project2 from "./assets/images/project/img2.png";
+import project3 from "./assets/images/project/img3.png";
+import person_project from "./assets/images/project/person.png";
 
 import avatar1 from "./assets/images/Testimonials/avatar1.png";
 import avatar2 from "./assets/images/Testimonials/avatar2.png";
